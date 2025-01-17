bf38a52540609f980b8b694a7d4879b1d2477c6b
aniboe <140517030+aniboe@users.noreply.github.com>
2025-01-17 16:57:33 +0000

    file

---
+++ files/datatype.js
@@ -0,0 +1,21 @@
+"use strict";//treats all js code as newer version
+
+// allert("hello")  //thid wont work here(not working on brouser)
+
+let name="anisur" //"string" can also be in single quotes('')
+let age=19;//int
+let isGay=false
+
+//number => 2^53(max number)
+//boollean => true/false
+//bigint(big int)
+//null => not empty, its a value itself
+//undefined => empty(value has not beenn assigned)
+//symbol => umiqueness (forgit what it means)
+
+//object 
+
+console.log(typeof"anisur")
+console.log(typeof"null")
+console.log(typeof"undefined")
+

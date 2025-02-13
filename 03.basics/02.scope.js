e9689546f22868d663b3429e9fb52f769038b1eb
aniboe <140517030+aniboe@users.noreply.github.com>
2025-02-13 12:31:24 +0000

    update

--- 03.basics/02.scope.js
+++ 03.basics/02.scope.js
@@ -16,8 +16,47 @@ let a = 300
 if (true) {
     let a = 10  
     const b = 20
-    console.log("inner:", a);// output: 10 for inside function
+    // console.log("inner:", a);// output: 10 for inside function
 }
 
-console.log("outer:", a);// output: 300 for outside function
+// console.log("outer:", a);// output: 300 for outside function
   
+//#################### nested scope
+
+
+
+function one(){
+    const username = "anisur"
+
+    function two(){
+        const website = "youtube"
+        console.log(username); 
+    }
+    // console.log(website); // cant access
+    two()
+}   
+
+// one()
+
+if (true){
+    const username = "anisur"
+    if (username === "anisur") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website); // cant access
+    
+}
+// console.log(username); // cant access
+
+//############################### interesting ##########################
+
+function addone(num){
+    return num + 1
+}
+
+console.log(addone(5));
+
+
+const addTwo =  function(num)
+

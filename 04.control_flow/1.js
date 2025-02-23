17efd5a7cf2c9b9711b57f0dc0d1c1980fdde893
aniboe <140517030+aniboe@users.noreply.github.com>
2025-02-23 14:07:13 +0000

    rename

---
+++ 04.control_flow/1.js
@@ -0,0 +1,16 @@
+// if
+const isLoggedIn = true
+
+if (isLoggedIn){
+    console.log("it works");
+    
+}
+
+
+//----------------Operator
+// <, >, <=, >=, ==, !=, === (also checks type of data)
+
+if (3 = "3 "){
+    console.log("it works");
+    
+}

1ab706c4cd1079d054bcd5026851315882db2143
aniboe <140517030+aniboe@users.noreply.github.com>
2025-02-26 08:57:04 +0000

    while

---
+++ 05.iteration/2.while.js
@@ -0,0 +1,23 @@
+
+// let index = 0
+// while (index <= 10){
+//     console.log(`value  of indes is ${index}`);
+//     index +=2
+// }
+
+
+myArray = ["batman","superman","ironman"]
+let arr = 0
+while (arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr++   
+}
+
+// ____________________________________________________________________
+
+
+let score = 1
+do{
+    console.log(score); 
+    score++
+}while(score <= 10)

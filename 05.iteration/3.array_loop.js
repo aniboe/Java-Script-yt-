6a6b30c607d4d8979c99e71ee27c6a6b7aad747e
aniboe <140517030+aniboe@users.noreply.github.com>
2025-02-26 09:29:13 +0000

    loops

---
+++ 05.iteration/3.array_loop.js
@@ -0,0 +1,61 @@
+//  for of
+
+// ["","",""]
+// [{},{},{}]
+
+
+
+// const array = [1,2,3,4,5]
+
+// for (const num of array) {
+//     console.log(num);
+// }
+
+
+
+// const greatings = "hello world"
+// for (const greet of greatings) {
+//     console.log(greet);
+    
+// }
+
+
+// ____________________________________________
+
+
+// Maps
+
+const map = new Map()  // it stores value in key value pair, has no repetion and stayes in same order ad we have given input
+
+map.set('in','india')
+map.set('usa','united states if america')
+map.set('fr','france')
+map.set('in','india')
+
+// console.log(map);
+
+// for (const mapkey of map) {
+//     console.log(mapkey);
+    
+// }
+
+// another method
+
+// for (const [mapKey, mapValue] of map) { //accessing ley value saperately
+//     console.log(mapKey, ":-",mapValue );
+// }
+
+
+
+
+// // following method doesnt work because object cant be iterated using this method (for of loop)
+// const myObject = {
+//     "game1" : "NFS",
+//     "game2" : "DGB",
+//     "game3" : "dont ask me i dont know"
+// }
+
+// for (const [key,value] of myObject) {
+//     console.log(key,":", value);
+// }
+

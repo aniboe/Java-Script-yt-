bf727df243276cb7696023edf6a6e1312ea578a8
aniboe <140517030+aniboe@users.noreply.github.com>
2025-03-14 07:53:24 +0000

    five

--- 05.iteration/5.five.js
+++ 05.iteration/5.five.js
@@ -1,7 +1,55 @@
 const coading = ["js","rb","py","java","cpp"]
 
 
-coading.forEach(function (item) {} )
+// coading.forEach( function (item) {}) // "item " will iterate the loop "coadeing"
+
+// coading.forEach(function_name(item){}) // error statement// we dont need to pass function name her "function" is datatpe declaration 
+
+// coading.forEach(function (values){
+//     console.log(values);
+// })
+
+// ---------------------------------------ARROW FUNCION // " () => {} "
+
+// coading.forEach((item) => {
+//     console.log(item);
+// })
+
+// ______________________________________________________________________________
+
+// function printme(value){
+//     console.log(value);
+// }
+
+// coading.forEach(printme())
+
+// _____________________________________________________________________
 
 
-3333333333333333333333333333333333333333333333333 WATCH AGAIN 
+// coading.forEach((item, index, arr) => { // (value / index / array that is being used)
+//     console.log(item, index, arr);    
+// })
+
+
+
+
+
+
+const mycoading = [
+    {
+        languageName : "java script",
+        languageFileName : "js",
+    },
+    {
+        languageName : "python",
+        languageFileName : "pyp",
+    },
+    {
+        languageName : "java",
+        languageFileName : "java",
+    }
+]
+mycoading.forEach( (item) => {
+    console.log(item.languageName); // "item" will go to each "object" and and look for "languageName"
+    console.log(item.languageFileName);
+})

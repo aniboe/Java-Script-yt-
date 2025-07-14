d3ed461202a7ec59a01d95305aacd371ce445451
aniboe <140517030+aniboe@users.noreply.github.com>
2025-07-14 12:00:47 +0000

    promise

--- 9.advance_one/2.promise.js
+++ 9.advance_one/2.promise.js
@@ -32,7 +32,7 @@ const promiseThree = new Promise(function(resolve, reject){
         })
     },1000)
 })
-const username = promiseThree.then(function(user){
+const user = promiseThree.then(function(user){
     console.log(user);
 })
 
@@ -41,7 +41,7 @@ const username = promiseThree.then(function(user){
 
 const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true
+        let error = true  // if true than it trigers error/else statment
         if(!error){
             resolve({username:'dono', password:'123'})
         }else{
@@ -49,6 +49,74 @@ const promiseFour = new Promise(function(resolve, reject){
         }
     },1000)
 })
-promiseFour.then((user) => {
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username
+})
+.then((username) => {   // returned value wilkl be passed inside this function
+    console.log(username);
+    
+})
+.catch(function(err){
+    console.log(err);
+    
+})
+.finally(() => {console.log('the primise is either resolved or rejected');
+})
 
+
+
+
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true  // if true than it trigers error/else statment /// if false than triger non error statement
+        if(!error){
+            resolve({username:'javascript', password:'123'})
+        }else{
+            reject('ERROR: JS went wrong')
+        }
+    },1000)
+})
+async function consumePromiseFive(){
+    // const response = await promiseFive
+    // console.log(response);
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+
+
+
+// async function getAllUser(){
+//     try {
+//         const response = await fetch('https://api.github.com/users/aniboe')
+//         // console.log(response);
+        
+//         const data = await response.json()
+//         console.log(data);
+    
+//     } catch (error) {
+//         console.log('ERROR:',error);
+        
+//     }
+// }getAllUser()
+
+
+
+
+fetch('https://api.github.com/users/aniboe')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {console.log(data);
+})
+.catch((error) => {console.log(error);
 })

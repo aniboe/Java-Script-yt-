c667705207f5e9894bc6f1d4d12e4144f0e90a18
aniboe <140517030+aniboe@users.noreply.github.com>
2025-07-24 08:37:44 +0000

    oop

--- 10.OOPs/02.oop.js
+++ 10.OOPs/02.oop.js
@@ -11,5 +11,30 @@ const user = {
         
     }
 }
-console.log(user.username);
+// console.log(user.username);
 // console.log(user.getUserDetails());
+
+
+//________________________________________________
+
+function User(username, loginCount, isloggegIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isloggegIn = isloggegIn
+
+    this.greetings = function(){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+
+    return this; // it happens implicitly so dont have to write it
+}
+// const user1 = User("anisur",12 , true)
+// const uaer2 = User("code and chil", 11, false) // this will over write above line even though the variable is diffrent
+// console.log(user1);
+
+
+const user1 = new User("anisur",12 , true)
+const user2 = new User("code and chil", 11, false) // by adding new keyword we resolve above issue and doesnt give excess details in terminal
+console.log(user1.constructor);
+// console.log(user2);

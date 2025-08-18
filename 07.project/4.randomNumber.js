6be232e7029707c5cbcbd98facd33d37342310f3
aniboe <140517030+aniboe@users.noreply.github.com>
2025-08-18 14:10:48 +0000

    partial

---
+++ 07.project/4.randomNumber.js
@@ -0,0 +1,49 @@
+const randumNumber = parseInt(Math.random() * 100 + 1)
+
+const submit = document.querySelector('#subt')
+
+const userInput = document.querySelector('#guessField')
+
+const remaining = document.querySelector('.lastResult')
+
+const lowOrHigh = document.querySelector('.lowOrHi')
+
+const startOver = document.querySelector('.resultParas')
+
+
+const p = document.createElement('p')
+
+let prevGues = []
+
+let numberGuess = 1 //guesses remaining
+
+let playGame = true
+
+
+
+function validateGues(guess){
+  // wether number is valid or not
+}
+
+function checkGuess(guess){
+  // wether guess is correct
+}
+
+function displayGuess(guess){
+// clear input
+  // add guesses in inner html
+  // decease number of play left
+}
+
+function displayMessag(message){
+  // is random number is low or high
+}
+
+function endaGame(){
+  // 
+}
+
+function newGame(){
+  // 
+}
+

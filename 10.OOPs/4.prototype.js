351c288cc9c5c444d3540931f1699803f2cc3146
aniboe <140517030+aniboe@users.noreply.github.com>
2026-03-01 22:01:31 +0530

    prototypal inheritance

--- 10.OOPs/4.prototype.js
+++ 10.OOPs/4.prototype.js
@@ -26,8 +26,52 @@ Array.prototype.hiAnisur = function(){
     
 }
 
+// print below to see if needed
 // heroPower.anisur()
-myHeros.anisur()
+// myHeros.anisur()
 
-myHeros.hiAnisur()
+// myHeros.hiAnisur()
 // heroPower.hiAnisur()
+
+
+
+// -------------------------------PROTOTYPAL INHERITANCE------------------------------
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSuport ={ 
+    isAvailable: true
+}
+
+const TASuport = {
+    makeAsignment: "js asignment",
+    fulTime: true,
+    __proto__: TeachingSuport // so now this object has access to TeachingSuport 
+}
+
+const user = {
+    name: "namai",
+    email: "namai@gmail.com"
+}
+
+// Teacher.__proto__ =  user // similar to above we can write this outside too
+// console.log(TASuport.isAvailable);
+// // __proto__ is not used much now (might find in old code base)
+// //  following is now way to write it 
+
+// // Object.setPrototypeOf(support to , suport of)
+// Object.setPrototypeOf(user, TASuport)
+// console.log(user.isAvailable);
+
+
+// MAKING A CUSTOM PROTOTYPE FOR STRING
+String.prototype.trueLength = function(){
+    console.log(this);
+    console.log(`${this}`);
+    console.log(`true length : ${this.trim().length}`)
+}
+
+const stri = " hihi  "
+stri.trueLength()

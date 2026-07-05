a622c3fc33f7033219a13262d1cea93a419cc447
aniboe <140517030+aniboe@users.noreply.github.com>
2026-07-05 14:45:11 +0530

    get set , behind the sean, not as inmportant as above , watch the video again

---
+++ 10.OOPs/12.defineProperty_get_set.js
@@ -0,0 +1,28 @@
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const ch = new User("lol@haha.lol", "lololol")
+
+console.log(ch); 
+console.log(ch.email); 
+console.log(ch.password); 
